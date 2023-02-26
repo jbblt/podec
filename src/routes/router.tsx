c010c8097e4781acca396147c12f97c5f94ea2a8
jbbalmont <jean-baptiste.balmont@expretio.com>
2023-02-25 20:45:21 -0500

    Use hashrouter

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,7 +2,7 @@ import App from 'App';
 import ErrorPage from 'pages/error/ErrorPage';
 import { ClientForm } from 'pages/Form/clientForm/ClientForm';
 import { ModelForm } from 'pages/Form/modelForm/ModelForm';
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 
 export enum RoutesPath {
   ROOT = '/',
@@ -10,20 +10,23 @@ export enum RoutesPath {
   FORM_MODEL = 'form/model',
 }
 
-export const router = createBrowserRouter([
-  {
-    path: RoutesPath.ROOT,
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: RoutesPath.FORM_CLIENT,
-        element: <ClientForm />,
-      },
-      {
-        path: RoutesPath.FORM_MODEL,
-        element: <ModelForm />,
-      },
-    ],
-  },
-]);
+export const router = createHashRouter(
+  [
+    {
+      path: RoutesPath.ROOT,
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: RoutesPath.FORM_CLIENT,
+          element: <ClientForm />,
+        },
+        {
+          path: RoutesPath.FORM_MODEL,
+          element: <ModelForm />,
+        },
+      ],
+    },
+  ],
+  { basename: '/' }
+);
